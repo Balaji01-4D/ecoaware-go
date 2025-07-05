cbe521ff1a9203bcba562e240a78f488843b3d14
Balaji01-4D <j.balaji2468@gmail.com>
2025-07-05 21:57:12 +0530

    feat: add updating, deleting user

--- controllers/adminController.go
+++ controllers/adminController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/Balaji01-4D/ecoware-go/initializer"
 	"github.com/Balaji01-4D/ecoware-go/models"
@@ -32,4 +33,53 @@ func GetAllUsers(c *gin.Context) {
 
 	c.JSON(http.StatusOK, users)
 
+}
+
+
+func DeleteUser(c *gin.Context) {
+	idParam := c.Param("id")
+
+	if err := initializer.DB.Delete(&models.User{}, idParam).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.Status(http.StatusOK)
+
+}
+
+
+func UpdateUser(c *gin.Context) {
+
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	var input struct {
+		Name  string	`json:"name"`
+		Email string	`json:"email"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	if err := initializer.DB.Model(&models.User{}).
+	Where("id = ?", id).
+	Update("name", input.Name).
+	Update("email", input.Email).
+	Error; err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusOK,gin.H{
+		"message":"updated successfully",
+	})
 }
