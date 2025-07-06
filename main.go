ad811384bb8d2717847d3064443697a088974e7e
Balaji01-4D <j.balaji2468@gmail.com>
2025-07-06 21:56:30 +0530

    feat: added a refresh token

--- main.go
+++ main.go
@@ -1,61 +1,12 @@
 package main
 
-import (
-	"time"
+import "github.com/Balaji01-4D/ecoware-go/routes"
 
-	"github.com/Balaji01-4D/ecoware-go/controllers"
-	"github.com/Balaji01-4D/ecoware-go/initializer"
-	"github.com/Balaji01-4D/ecoware-go/middleware"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-)
 
-func init() {
-	initializer.LoadEnvVariables()
-	initializer.ConnectToDB()
-}
+
 
 func main() {
-	r := gin.Default()
 
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8080", "http://127.0.0.1:8080"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "this is home",
-		})
-	})
-	r.POST("/auth/register", controllers.RegisterUser)
-	r.POST("/auth/login", controllers.Login)
-	r.GET("/auth/me", controllers.Me)
-	r.GET("/categories", controllers.GetAllCategories)
-
-	protected := r.Group("/")
-	protected.Use(middleware.RequireAuth())
-
-	protected.GET("/complaints", controllers.GetMyComplaints)
-	protected.POST("/complaints", controllers.AddComplaints)
-	protected.GET("/complaints/:id", controllers.GetComplaintByID)
-	protected.PUT("/user/password", controllers.UpdatePassword)
-	protected.PUT("/user/profile", controllers.UpdateUserByUser)
-
-
-	admin := r.Group("/admin")
-	admin.Use(middleware.RequireAuth(), middleware.RequireAdmin())
-
-	admin.GET("/users", controllers.GetAllUsers)
-	admin.DELETE("/users/:id", controllers.DeleteUser)
-	admin.PUT("/users/:id", controllers.UpdateUser)
-	admin.GET("/complaints", controllers.GetAllComplaints)
-	admin.PUT("/complaints/:id/status", controllers.UpdateComplaintStatus)
-
-
+	r := routes.SetupRouter()
 	r.Run()
 }
--- models/refreshToken.go
+++ models/refreshToken.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type RefreshSession struct {
-	ID	uint			`gorm:"primaryKey"`
-	UserId uint			`gorm:"index"`
-	Token	string		`gorm:"uniqueIndex"`
+	ID        uint   `gorm:"primaryKey"`
+	UserID    uint   `gorm:"index"`
+	Token     string `gorm:"uniqueIndex"`
 	ExpiresAt time.Time
 	CreatedAt time.Time
 }
