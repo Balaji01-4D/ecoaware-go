aa5410d238b667785b22d50fdedddb4ce6d5cc0e
Balaji01-4D <j.balaji2468@gmail.com>
2025-07-06 21:57:45 +0530

    added a refresh token

--- initializer/LoadEnvVariables.go
+++ initializer/LoadEnvVariables.go
@@ -1,14 +1,16 @@
 package initializer
 
-import	(
-	"github.com/joho/godotenv"
+import (
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatal("Error loading the .env files")
+		log.Fatal("Error loading the .env files", err)
+		
 	}
 }
